dd0635d4159f0caa81a2c31540818389b308e180
LAPTOP-RFDL1QJT\ASUS <idinmaster.1004.2015@gmail.com>
2024-03-01 18:24:13 +0700

    Intitial dentist project

---
+++ models/Dentist.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const DentistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true,'Please add a name'],
+        unique: true,
+        trim: true,
+        maxlength:[50,'Name can not be more than 50 characters']
+    },
+    years_of_experience:{
+        type: String,
+        required: [true,'Please add years of experience']
+    },
+    area_of_expertise:{
+        type: String,
+        required: [true,'Please add an area of expertise']
+    }
+},{
+    toJSON: {virtuals:true},
+    toObject:{virtuals:true}
+});
+
+//Reverse populate with virtuals
+DentistSchema.virtual('appointments',{
+    ref: 'Appointment',
+    localField: '_id',
+    foreignField:'dentist',
+    justOne:false
+});
+
+//Cascade delete appointments when a hospitla is deleted
+DentistSchema.pre('deleteOne',{document:true, query: false}, async function(next){
+    console.log(`Appointments being removed from dentist ${this._id}`);
+    await this.model('Appointment').deleteMany({dentist: this._id});
+    next();
+});
+
+module.exports=mongoose.model('Dentist',DentistSchema);
---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cookieParser=require('cookie-parser');
+const connectDB = require('./config/db');
+
+//Load env vars
+dotenv.config({path:'./config/config.env'});
+connectDB();
+
+//Route files
+const dentists = require('./routes/dentists');
+const appointments = require('./routes/appointments');
+const auth = require('./routes/auth');
+
+const app=express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+//Mount routers
+app.use('/api/v1/dentists' ,dentists);
+app.use('/api/v1/appointments', appointments);
+app.use('/api/v1/auth',auth);
+
+const PORT=process.env.PORT || 5000;
+const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection',(err,promise)=>{
+    console.log(`Error: ${err.message}`);
+    server.close(()=>process.exit(1));
+});
