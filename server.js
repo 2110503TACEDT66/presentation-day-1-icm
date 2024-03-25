ab26451cf01d82e0a53ff3d22bd3e71bbafacb66
LAPTOP-RFDL1QJT\ASUS <idinmaster.1004.2015@gmail.com>
2024-03-25 18:57:06 +0700

    Add vercel

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.use('/api/v1/appointments', appointments);
 app.use('/api/v1/auth',auth);
 
 const PORT=process.env.PORT || 5000;
-const server = app.listen(PORT, console.log('Server running in ', process.env.NODE_ENV, ' mode on port ', PORT));
+const server = app.listen(PORT, console.log('Server running in ', 
+process.env.NODE_ENV, 'on ' + process.env.HOST + ":" + PORT));
 
 //Handle unhandled promise rejections
 process.on('unhandledRejection',(err,promise)=>{
